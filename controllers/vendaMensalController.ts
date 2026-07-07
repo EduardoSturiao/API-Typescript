ee6ceb89f62aabf2ae210b59346c026fef73dc9d
Eduardo <sturiaoeduardo@gmail.com>
2026-07-07 16:49:33 -0300

    add vendaMensal controller

--- controllers/vendaMensalController.ts
+++ controllers/vendaMensalController.ts
@@ -1,19 +1,54 @@
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
+// import type { RequestHandler } from "express"
+//obs pra req e res: RequestHandler poupa tempo
 import VendaMensal from "../models/VendaMensal.ts";
 
-export const listar = (req: Request, res: Response): void => {
+interface IFiltroVenda {
+  mes?: number;
+}
+
+export const listar = (req: Request, res: Response) => {
   res.send("Bem vindo ao banco de dados");
 };
 
-export const allVendas = async (req: Request,  res: Response) =>{
-    try {
-        const vendasMensais = await VendaMensal.find()
-    } catch (error) {
-        
+export const allVendas = async (req: Request, res: Response) => {
+  try {
+    const filtro: IFiltroVenda = {};
+
+    if (req.query.mes) {
+      filtro.mes = Number(req.query.mes);
     }
-}
- 
-export const atualizar = async (req: Request, res: Response): Promise<void> => {
+
+    const limite = req.query.limite ? Number(req.query.limite) : 10;
+    const ordem = req.query.ordem === "maior" ? -1 : 1;
+
+    const pagina = req.query.pagina ? Number(req.query.pagina) : 1;
+    const skip = (pagina - 1) * limite;
+
+    const vendasMensais = await VendaMensal.find(filtro)
+      .sort({ valorVendido: ordem })
+      .skip(skip)
+      .limit(limite)
+      .populate("cliente");
+
+    const total = await VendaMensal.countDocuments(filtro);
+
+    res.json({
+      pagina,
+      totalPaginas: Math.ceil(total / limite),
+      totalResultados: total,
+      resultados: vendasMensais,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ mensagem: error.message });
+    } else {
+      res.status(500).json({ mensagem: "Erro desconhecido" });
+    }
+  }
+};
+
+export const atualizar = async (req: Request, res: Response) => {
   try {
     const attVendaMensal = await VendaMensal.findByIdAndUpdate(
       req.params.id,
@@ -35,18 +70,18 @@ export const atualizar = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+export const deletar = async (req: Request, res: Response) => {
+  try {
+    const vendaMensalExcluida = await VendaMensal.findByIdAndDelete(
+      req.params.id,
+    );
 
-export const deletar = async (req: Request, res: Response) =>{
-try {
-    const vendaMensalExcluida = await VendaMensal.findByIdAndDelete(req.params.id)
-
-
-    if(!vendaMensalExcluida){
-        return res.status(404).json({mensagem: "Venda mensal não encontrada"})
+    if (!vendaMensalExcluida) {
+      return res.status(404).json({ mensagem: "Venda mensal não encontrada" });
     }
 
-    res.json(vendaMensalExcluida)
-} catch (error) {
+    res.json(vendaMensalExcluida);
+  } catch (error) {
     if (error instanceof Error) {
       res.status(400).json({ mensagem: error.message });
     } else {
@@ -54,4 +89,3 @@ try {
     }
   }
 };
-
--- server.ts
+++ server.ts
@@ -1,3 +1,17 @@
-import { connectDB } from "./config/db.ts"
+import { connectDB } from "./config/db.ts";
 
-connectDB()
+connectDB();
+
+class Person {
+  age: number;
+  name: string;
+  email: string;
+
+  constructor(age: number, name: string, email: string){
+this.age = age,
+this.name = name,
+this.email = email
+  }
+}
+
+const person = new Person(24, 'Eduardo', 'teste@')
