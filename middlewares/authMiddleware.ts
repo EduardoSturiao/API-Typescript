844daca3db1bee2012c9aa8ff48567d1df4049fa
Eduardo <sturiaoeduardo@gmail.com>
2026-07-09 23:35:52 -0300

    add types

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -10,11 +10,19 @@ export const protegerRota = (req: Request, res: Response, next: NextFunction)=>{
     }
 
     const token = authHeader.split(' ')[1]
-
+    
     try {
         const dadosDecodificados = jwt.verify(token as string, process.env.JWT_SECRET as string)
-        req.usuarioId = dadosDecodificados.id
+
+
+        if(typeof dadosDecodificados === 'string'){
+            return res.status(401).json({mensagem: 'Token inválido'})
+        }
+
+        req.usuarioId = dadosDecodificados.id as string
         next()
+        
+
     } catch (error) {
         if(error instanceof Error){
             res.status(401).json({mensagem: 'Token inválido ou expirado'})
